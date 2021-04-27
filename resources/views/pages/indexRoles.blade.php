f3fa574863911fb09edf33364c1ae394da45e223
Triyana Group <Triyana Group@DESKTOP-DJTTJJ9>
2021-04-27 09:47:12 +0700

    iii

---
+++ resources/views/pages/indexRoles.blade.php
@@ -0,0 +1,43 @@
+@extends('layout.backEnd')
+@section('container')
+<div class="container mt-5">
+  <div class="col-md-12 mt-5">
+    <div class="card">
+      <div class="card-header">
+        <h2>Data Roles Management</h2>
+      </div>
+      <div class="card-body">
+      <a href="#" data-action="/home"><< Back</a> <!--|| <a href="#" data-action="create">Add</a>--> || <a href="#" data-action="/roles/trash">Trashed</a>
+        <div class="table-responsive">
+          <table style="width: 100%;" id="tabroles" data-action="/roles/table" class="table table-md table-hover table-striped table-bordered">
+            <thead><tr>
+                <th scope="col">No</th>
+                <th scope="col">Name</th>
+                <th scope="col">Action</th>
+            </tr></thead>
+            <tbody>
+            @php
+            $i=1;
+            @endphp
+            @if (count($roles) > 0 )
+            @foreach ($roles as $key => $role)
+            <tr>
+              <td>{{ $i++ }}</td>
+              <td>{{ $role->name }}</td>
+              <td>
+                <a href="#" data-type="editCustomer" data-action="edit" data-id="{{ $role->id }}">Edit</a> ||
+                <a href="#" data-type="deleteCustomer" data-action="delete" data-id="{{ $role->id }}">Delete</a>
+              </td>
+            </tr>
+            @endforeach
+            @else
+            <tr><td colspan="7" align="center">Data Tidak Ditemukan</td></tr>
+            @endif
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
