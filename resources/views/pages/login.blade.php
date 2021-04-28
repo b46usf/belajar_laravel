d893be9ebed5b837be4d0c2386d1d357e2367b17
Triyana Group <Triyana Group@DESKTOP-DJTTJJ9>
2021-04-28 14:37:10 +0700

    iii

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -10,11 +10,9 @@
                 @csrf
                 <div class="card-body">
                     @if(session('errors'))
-                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <div class="alert alert-warning alert-dismissible fade show" role="alert">
                             Something it's wrong:
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">×</span>
-                            </button>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                             <ul>
                             @foreach ($errors->all() as $error)
                             <li>{{ $error }}</li>
--- resources/views/template/mainAuth.blade.php
+++ resources/views/template/mainAuth.blade.php
@@ -9,4 +9,7 @@
 <body>
 @yield('content')
 </body>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js"></script>
 </html>
