7d40876c8e3b1941dac8d5b213a96895096b7a34
Triyana Group <Triyana Group@DESKTOP-DJTTJJ9>
2021-05-03 05:28:00 +0700

    iii

--- resources/views/layout/backEnd.blade.php
+++ resources/views/layout/backEnd.blade.php
@@ -1,6 +1,6 @@
 @extends('template.mainDashboard')
 @section('content')
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="#">
             <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
