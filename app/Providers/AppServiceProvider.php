5ac1555a27dd208d139c1a4934660156cc4a3fd1
Triyana Group <Triyana Group@DESKTOP-DJTTJJ9>
2021-05-04 05:05:14 +0700

    iii

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Pages;
+use Spatie\Permission\Models\Role;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +28,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $pages  = Pages::get();//dd($pages);
+        $id     = Auth::user();dd($id);
+        $akses  = DB::table('model_has_roles')->join('users', 'users.id', '=', 'model_has_roles.model_id')->where('users.uniqID_user',$id)->get(); 
+        foreach ($akses as $key => $value) {
+            if ($value->role_id==1) {
+                $pages  = Pages::get();
+            }
+        }
     }
 }
