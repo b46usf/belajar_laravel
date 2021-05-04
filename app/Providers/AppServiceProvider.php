3bd1d53513580ecbfde3a0b16214ea28f2feb877
Triyana Group <Triyana Group@DESKTOP-DJTTJJ9>
2021-05-04 12:58:23 +0700

    iii

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,9 +6,6 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
-use App\Models\User;
-use App\Models\Pages;
-use Spatie\Permission\Models\Role;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,13 +26,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $pages  = Pages::get();dd(Auth::check());
-        // $id     = Auth::user();dd($id);
-        // $akses  = DB::table('model_has_roles')->join('users', 'users.id', '=', 'model_has_roles.model_id')->where('users.uniqID_user',$id)->get(); 
-        // foreach ($akses as $key => $value) {
-        //     if ($value->role_id==1) {
-        //         $pages  = Pages::get();
-        //     }
-        // }
+        view()->composer('*', function ($view) 
+        {
+            if (Auth::check()) {
+                $akses  =   DB::table('model_has_roles')
+                ->join('users', 'users.id', '=', 'model_has_roles.model_id')
+                ->join('roles', 'roles.id', '=', 'model_has_roles.role_id')
+                ->where('users.uniqID_user',Auth::user()->uniqID_user)->first();
+                $permissions    =   
+                DB::table('permissions')
+                ->select(DB::raw("distinct SUBSTRING_INDEX(name, '-', 1) as pagepermission"))
+                ->join('role_has_permissions', 'role_has_permissions.permission_id', '=', 'permissions.id')
+                ->where('role_has_permissions.role_id', '=', $akses->role_id);
+                $pages  =
+                DB::table('pages')
+                ->select(DB::raw('distinct pages.id,pages.name,pages.url'))
+                ->joinSub($permissions, 'permissions', function ($join) {
+                    $join->on('pages.name', '=', 'permissions.pagepermission');
+                }); 
+                $results    =   $pages->get();
+                $view->with('pages', $results );
+            }
+        });
     }
 }
